7dd0a340d6feb8bf3ef6ef980e014ff8ba386383
jmpark6846 <jmpark6846@gmail.com>
2019-04-15 07:44:53 +0900

    예제6 추가

--- ex6/ex6.py
+++ ex6/ex6.py
@@ -1,1 +1,35 @@
+import argparse
+import os
+import subprocess
 
+parser = argparse.ArgumentParser()
+parser.add_argument('path')
+parser.add_argument('-name')
+parser.add_argument('-type')
+parser.add_argument('-print', action="store_true")
+parser.add_argument('-exec')
+
+args = parser.parse_args()
+
+def run_command(entry, _print=None, exec=None):
+    print(entry, _print, exec)
+    if _print:
+        print(entry.name)
+
+    if exec:
+        subprocess.run([exec, ])
+
+def find_files_in_dir(path, name=None, type=None, _print=None, exec=None):
+    with os.scandir(path=path) as it:
+        for entry in it:
+            if name:
+                if name in entry.path.split('/')[-1]:
+                    run_command(entry, _print=_print, exec=exec)
+            else:
+                run_command(entry, _print=_print, exec=exec)
+
+            if entry.is_dir():
+                find_files_in_dir(entry.path, name)
+
+
+find_files_in_dir(args.path, args.name, args.type, args.print, args.exec)
---
+++ ex6/readme.md
@@ -0,0 +1,25 @@
+# find 명령어 구현하기
+
+- [x] path 받기
+- [x] -name 구현하기
+- [ ] name 에서 * 같은 조건 구현하기
+- [ ] -type 구현하기
+- [x] -print 구현하기
+- [ ] -exec 구현하기
+
+- subprocess로 실행
+- os.scandir 로 파일 출력하기
+
+
+    -type t
+    
+     True if the file is of the specified type.  Possible file types are as follows:
+    
+     b       block special
+     c       character special
+     d       directory
+     f       regular file
+     l       symbolic link
+     p       FIFO
+     s       socket
+
